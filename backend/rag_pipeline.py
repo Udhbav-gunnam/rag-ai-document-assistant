2f928b51a9f9f063bc9a4927e2f883fd7b899b55
Udhbav.G <udhbav23@gmail.com>
2026-04-05 23:42:54 +0530

    Implemented working RAG pipeline with secure environment configuration

--- backend/rag_pipeline.py
+++ backend/rag_pipeline.py
@@ -3,28 +3,31 @@ load_dotenv()
 
 import os
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import PyPDFLoader
+from langchain_groq import ChatGroq
 
+
+# Global vector database
 vector_db = None
 
 
 def process_document(file):
-
     global vector_db
 
+    # Save uploaded file temporarily
     file_location = f"temp_{file.filename}"
 
     with open(file_location, "wb") as f:
         f.write(file.file.read())
 
+    # Load PDF
     loader = PyPDFLoader(file_location)
     documents = loader.load()
 
+    # Split document into chunks
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
         chunk_overlap=50
@@ -32,30 +35,46 @@ def process_document(file):
 
     chunks = splitter.split_documents(documents)
 
+    # Create embeddings
     embeddings = HuggingFaceEmbeddings()
 
+    # Store inside FAISS vector database
     vector_db = FAISS.from_documents(chunks, embeddings)
 
     return "Document processed successfully"
 
 
 def ask_question(question):
-
     global vector_db
 
     if vector_db is None:
         return "Please upload a document first."
 
+    # Initialize Groq LLM
     llm = ChatGroq(
         groq_api_key=os.getenv("GROQ_API_KEY"),
-        model="mixtral-8x7b-32768"
+        model="llama-3.3-70b-versatile"
     )
 
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=vector_db.as_retriever()
-    )
+    # Retrieve relevant document chunks
+    retriever = vector_db.as_retriever()
 
-    response = qa_chain.run(question)
+    docs = retriever.invoke(question)[:4]
 
-    return response
+    # Combine retrieved context
+    context = "\n".join([doc.page_content for doc in docs])
+
+    # Create prompt
+    prompt = f"""
+Use the following context to answer the question:
+
+{context}
+
+Question:
+{question}
+"""
+
+    # Generate response
+    response = llm.invoke(prompt)
+
+    return response.content
